9164034c3182435c0128a59cd0ab19a6f9b81abc
ShengHuaWu <swu1@wayfair.com>
2019-11-29 11:22:53 +0100

    Try out argument parsing.

--- my-script.swift
+++ my-script.swift
@@ -2,8 +2,9 @@
 
 import Foundation
 
+// For dependency management, please read https://github.com/mxcl/swift-sh
 // External dependencies
-import ShellOut // @JohnSundell ~> 2.2.0
+import TSCUtility // https://github.com/apple/swift-package-manager == 528da51
 
 // Local dependencies
 import MyLib1 // ./MyLib1
@@ -13,6 +14,44 @@ import MyLib1 // ./MyLib1
 
 justAFunction() // This is from `MyLib1`
 
+// Arguments (options) parsing
+// For more parsing rules, please read https://rderik.com/blog/command-line-argument-parsing-using-swift-package-manager-s/
+do {
+    let parser = ArgumentParser(commandName: "ap",
+                                usage: "arguments parsing",
+                                overview: "The command is used for argument parsing")
+    
+    let enable = parser.add(option: "--enable",
+                            shortName: "-e",
+                            kind: Bool.self,
+                            usage: "Enable something",
+                            completion: ShellCompletion.none)
+    let input = parser.add(option: "--input",
+                           shortName: "-i",
+                           kind: String.self,
+                           usage: "An input filename",
+                           completion: .filename)
+    
+    let argsv = Array(CommandLine.arguments.dropFirst())
+    let parguments = try parser.parse(argsv)
+    
+    if let isEnabled = parguments.get(enable) {
+        print("Enabled: \(isEnabled)")
+    }
+    
+    if let filename = parguments.get(input) {
+        print("Using filename: \(filename)")
+    }
+
+} catch ArgumentParserError.expectedValue(let value) {
+    print("Missing value for argument \(value).")
+} catch ArgumentParserError.expectedArguments(let parser, let stringArray) {
+    print("Parser: \(parser) Missing arguments: \(stringArray.joined()).")
+} catch {
+    print(error.localizedDescription)
+}
+
+// Basic functionalities
 let downloadsDir = "~/Downloads"
 let newTempDir = "NewTemp"
 let newFile = "my-file.txt"
@@ -58,8 +97,8 @@ func tryProcess() throws {
     print(try run(command: "python", arguments: ["--version"]))
 }
 
-do {
-    try tryProcess()
-} catch let error {
-    print("Error occurs: \n\(error.localizedDescription)")
-}
+//do {
+//    try tryProcess()
+//} catch let error {
+//    print("Error occurs: \n\(error.localizedDescription)")
+//}
