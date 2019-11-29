a108a88e48de52f860f1225d465ab135584d5f35
ShengHuaWu <swu1@wayfair.com>
2019-11-29 10:58:46 +0100

    Remove shellout code.

--- my-script.swift
+++ my-script.swift
@@ -20,56 +20,6 @@ let newFile1 = "my-file1.txt"
 let newFile2 = "my-file2.txt"
 let newText = "this is a good text."
 
-func tryShellOut() throws {
-    let help1 = try shellOut(to: "sh", arguments: ["adb-run-tests.sh", "-h"], at: "~/Development/swift-everywhere-toolchain/Platypus")
-    print(help1)
-    
-    let help2 = try shellOut(to: "make", arguments: ["help"], at: "~/Development/swift-everywhere-toolchain")
-    print(help2)
-    
-    let mkdir = try shellOut(to: "mkdir", arguments: ["-p", newTempDir], at: downloadsDir)
-    print(mkdir)
-    
-    let touch = try shellOut(to: "touch", arguments: [newFile], at: "\(downloadsDir)/\(newTempDir)")
-    print(touch)
-    
-    let echo = try shellOut(to: "echo", arguments: [newText, ">>", newFile], at: "\(downloadsDir)/\(newTempDir)")
-    print(echo)
-    
-    let cat1 = try shellOut(to: "cat", arguments: [newFile], at: "\(downloadsDir)/\(newTempDir)")
-    print(cat1)
-    
-    let ls1 = try shellOut(to: "ls", arguments: ["-al"], at: "\(downloadsDir)/\(newTempDir)")
-    print(ls1)
-    
-    let cp = try shellOut(to: "cp", arguments: [newFile, newFile1], at: "\(downloadsDir)/\(newTempDir)")
-    print(cp)
-    
-    let ls2 = try shellOut(to: "ls", arguments: ["-al"], at: "\(downloadsDir)/\(newTempDir)")
-    print(ls2)
-    
-    let cat2 = try shellOut(to: "cat", arguments: [newFile1], at: "\(downloadsDir)/\(newTempDir)")
-    print(cat2)
-    
-    let mv = try shellOut(to: "mv", arguments: [newFile, newFile2], at: "\(downloadsDir)/\(newTempDir)")
-    print(mv)
-    
-    let ls3 = try shellOut(to: "ls", arguments: ["-al"], at: "\(downloadsDir)/\(newTempDir)")
-    print(ls3)
-    
-    let cat3 = try shellOut(to: "cat", arguments: [newFile2], at: "\(downloadsDir)/\(newTempDir)")
-    print(cat3)
-    
-    let rm = try shellOut(to: "rm", arguments: ["*"], at: "\(downloadsDir)/\(newTempDir)")
-    print(rm)
-    
-    let rmdir = try shellOut(to: "rmdir", arguments: [newTempDir], at: downloadsDir)
-    print(rmdir)
-    
-    let ls4 = try shellOut(to: "ls", arguments: ["-al"], at: downloadsDir)
-    print(ls4)
-}
-
 func run(command: String, arguments: [String] = [], at path: String = ".") throws -> String {
     let process = Process()
     let pipe = Pipe()
@@ -109,7 +59,6 @@ func tryProcess() throws {
 }
 
 do {
-//    try tryShellOut()
     try tryProcess()
 } catch let error {
     print("Error occurs: \n\(error.localizedDescription)")
