6040899ad15c28e32f20b295b7ba2c5121aa868c
ShengHua Wu <fantasy0404@gmail.com>
2019-12-15 22:47:49 +0100

    Set up Haskell bash script.

---
+++ Haskell/my-script.hs
@@ -0,0 +1,7 @@
+#!/usr/bin/env runhaskell
+                                    -- #!/bin/bash
+{-# LANGUAGE OverloadedStrings #-}  --
+                                    --
+import Turtle                       --
+                                    --
+main = echo "Hello, world!"         -- echo Hello, world!
