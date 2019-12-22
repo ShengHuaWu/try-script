f736cb0955fe261f7aa375f9c83c217e7419e43f
ShengHua Wu <fantasy0404@gmail.com>
2019-12-22 15:04:34 +0100

    Simple Haskell function.

--- Haskell/my-script.hs
+++ Haskell/my-script.hs
@@ -1,7 +1,14 @@
 #!/usr/bin/env runhaskell
-                                    -- #!/bin/bash
-{-# LANGUAGE OverloadedStrings #-}  --
-                                    --
-import Turtle                       --
-                                    --
-main = echo "Hello, world!"         -- echo Hello, world!
+
+{-# LANGUAGE OverloadedStrings #-}
+
+-- https://hackage.haskell.org/package/turtle-1.5.15/docs/Turtle-Tutorial.html
+
+import Turtle
+
+myFunc :: Integer -> Integer
+myFunc i = i + 1
+
+main = do
+  dir <- pwd
+  print dir
