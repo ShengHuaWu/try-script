2f23a7adee9fd914beeedc959946e1513eb400e8
ShengHua Wu <fantasy0404@gmail.com>
2019-12-27 21:41:37 +0100

    Install turtle locally with stack. Creare a preload file for REPL.

--- Haskell/my-script.hs
+++ Haskell/my-script.hs
@@ -9,6 +9,13 @@ import Turtle
 myFunc :: Integer -> Integer
 myFunc i = i + 1
 
-main = do
+-- The <- symbol is overloaded and its meaning is context-dependent; in this context it just means "store the current result"
+-- The = symbol is not overloaded and always means that the two sides of the equality are interchangeable
+datePwd = do
   dir <- pwd
-  print dir
+  datefile dir -- do notation implicitly returns the value of the last command within a subroutine
+
+main = do
+  time <- datePwd
+  print time
+  print( "123" <> "789" ) -- `<>` is string concatenation
