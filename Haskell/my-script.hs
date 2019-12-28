36c8d3743772c6e36dd6040e59c2f932e6b6b259
ShengHua Wu <fantasy0404@gmail.com>
2019-12-28 15:38:25 +0100

    Add type signature and try shell function.

--- Haskell/my-script.hs
+++ Haskell/my-script.hs
@@ -11,11 +11,18 @@ myFunc i = i + 1
 
 -- The <- symbol is overloaded and its meaning is context-dependent; in this context it just means "store the current result"
 -- The = symbol is not overloaded and always means that the two sides of the equality are interchangeable
+datePwd :: IO UTCTime
 datePwd = do
   dir <- pwd
   datefile dir -- do notation implicitly returns the value of the last command within a subroutine
 
+main :: IO () -- Type signature (not necessary)
 main = do
-  time <- datePwd
-  print time
-  print( "123" <> "789" ) -- `<>` is string concatenation
+  -- time <- datePwd
+  -- print time
+  -- print( "123" <> "789" ) -- `<>` is string concatenation
+  let cmd = "false"
+  x <- shell cmd empty
+  case x of
+      ExitSuccess   -> return ()
+      ExitFailure n -> die (cmd <> " failed with exit code: " <> repr n)
